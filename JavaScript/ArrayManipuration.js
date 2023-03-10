b2f8e4e39835483b43d54790b11942231d746aee
haloyukka <gm.halohalo@gmail.com>
2023-03-10 11:42:53 +0900

    JavaScriptコードを追加

---
+++ JavaScript/ArrayManipuration.js
@@ -0,0 +1,15 @@
+[4, 5, 6, 7].at(1);
+[4, 5, 6, 7].push(8);
+[4, 5, 6, 7].pop();
+[4, 5, 6, 7].fill(1);
+[4, 5, 6, 7].join(' ');
+[4, 5, 6, 7].shift();
+[4, 5, 6, 7].reverse();
+[4, 5, 6, 7].unshift(3);
+[4, 5, 6, 7].includes(6);
+[4, 5, 6, 7].map(item => 2*item);
+[4, 5, 6, 7].filter(item => item > 5);
+[4, 5, 6, 7].find(item => item > 5);
+[4, 5, 6, 7].every(item => item > 0);
+[4, 5, 6, 7].findIndex(item => item === 5);
+[4, 5, 6, 7].reduce((prev, curr) => prev+curr, 0);
